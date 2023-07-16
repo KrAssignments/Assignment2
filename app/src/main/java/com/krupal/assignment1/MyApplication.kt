f4986625f7691061362db36db0b2878f270bda5d
Krupal Bhardiya <krbhardiya@gmail.com>
2023-07-17 00:20:18 +0530

    HiltApp defined with searchable activity
    Signed-off-by: Krupal Bhardiya <krbhardiya@gmail.com>

---
+++ app/src/main/java/com/krupal/assignment1/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.krupal.assignment1
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : MultiDexApplication() {
+}
